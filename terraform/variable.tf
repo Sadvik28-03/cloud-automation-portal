f338a835451a5f4155e18139073a698c14fdc0a0
Sadvik28-03 <202117b3706@wilp.bits-pilani.ac.in>
2026-01-31 22:51:08 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,9 +1,9 @@
 variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
+  description = "AWS region selected by user"
+  type        = string
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t3.micro"
+  description = "EC2 instance type selected by user"
+  type        = string
 }
