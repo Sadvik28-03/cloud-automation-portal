4c33b697008c87dca8205016cf3fb8dd5d3dffb5
Sadvik28-03 <202117b3706@wilp.bits-pilani.ac.in>
2026-01-31 22:51:35 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,17 @@ provider "aws" {
   region = var.region
 }
 
+variable "ami_map" {
+  type = map(string)
+  default = {
+    us-east-1  = "ami-0c02fb55956c7d316"
+    ap-south-1 = "ami-0f5ee92e2d63afc18"
+    us-west-2  = "ami-03f65b8614a860c29"
+  }
+}
+
 resource "aws_instance" "demo" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = var.ami_map[var.region]
   instance_type = var.instance_type
 
   tags = {
